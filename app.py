c3de9d85a8a3b54e405660c923e164c9ab215063
Krish <200975760+krishachra@users.noreply.github.com>
2025-12-05 20:49:33 +0530

    added login page

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+
+
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        # For testing:
+        print(username, password)
+
+        # You can add real authentication here later
+
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
